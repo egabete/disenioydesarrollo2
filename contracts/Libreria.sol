b95988ad46f09d83993f355a3b3177b4b2b5f3e3
Esteban Abete <esteban.abete@gmail.com>
2019-10-28 04:12:18 +0100

    changes to prices en Libreria

--- contracts/Libreria.sol
+++ contracts/Libreria.sol
@@ -1,8 +1,6 @@
 pragma solidity >=0.4.21 <0.6.0;
 
 
-
-
 contract LibreriaData {
 
     uint256 internal constant  maxAmmountOfBooks = 20;
@@ -37,147 +35,144 @@ contract LibreriaData {
                 books[0].bookId = 0;
                 books[0].owner = msg.sender;
                 books[0].temporalOwner = msg.sender;
-                books[0].purchasePrice = 10000000000000;
-                books[0].lendingPrice = 5000000000000;
+                books[0].purchasePrice = 100000000000000;
+                books[0].lendingPrice = 500000000000000;
                 bookIndex.push(books[0].bookId);
 
                 books[1].bookId = 1;
                 books[1].owner = msg.sender;
                 books[1].temporalOwner = msg.sender;
-                books[1].purchasePrice = 10000000000000;
-                books[1].lendingPrice = 5000000000000;
+                books[1].purchasePrice = 100000000000000;
+                books[1].lendingPrice = 500000000000000;
                 bookIndex.push(books[1].bookId);
-                
+
                 books[2].bookId = 2;
                 books[2].owner = msg.sender;
                 books[2].temporalOwner = msg.sender;
-                books[2].purchasePrice = 10000000000000;
-                books[2].lendingPrice = 5000000000000;
+                books[2].purchasePrice = 100000000000000;
+                books[2].lendingPrice = 500000000000000;
                 bookIndex.push(books[2].bookId);
-                
+
                 books[3].bookId = 3;
                 books[3].owner = msg.sender;
                 books[3].temporalOwner = msg.sender;
-                books[3].purchasePrice = 10000000000000;
-                books[3].lendingPrice = 5000000000000;
+                books[3].purchasePrice = 100000000000000;
+                books[3].lendingPrice = 500000000000000;
                 bookIndex.push(books[3].bookId);
-                
+
                 books[4].bookId = 4;
                 books[4].owner = msg.sender;
                 books[4].temporalOwner = msg.sender;
-                books[4].purchasePrice = 10000000000000;
-                books[4].lendingPrice = 5000000000000;
+                books[4].purchasePrice = 100000000000000;
+                books[4].lendingPrice = 500000000000000;
                 bookIndex.push(books[4].bookId);
-                
+
                 books[5].bookId = 5;
                 books[5].owner = msg.sender;
                 books[5].temporalOwner = msg.sender;
-                books[5].purchasePrice = 10000000000000;
-                books[5].lendingPrice = 5000000000000;
+                books[5].purchasePrice = 100000000000000;
+                books[5].lendingPrice = 500000000000000;
                 bookIndex.push(books[5].bookId);
-                
+
                 books[6].bookId = 6;
                 books[6].owner = msg.sender;
                 books[6].temporalOwner = msg.sender;
-                books[6].purchasePrice = 10000000000000000;
-                books[6].lendingPrice = 5000000000000000;
+                books[6].purchasePrice = 100000000000000000;
+                books[6].lendingPrice = 500000000000000000;
                 bookIndex.push(books[6].bookId);
-                
+
                 books[7].bookId = 7;
                 books[7].owner = msg.sender;
                 books[7].temporalOwner = msg.sender;
-                books[7].purchasePrice = 10000000000000;
-                books[7].lendingPrice = 5000000000000;
+                books[7].purchasePrice = 100000000000000;
+                books[7].lendingPrice = 500000000000000;
                 bookIndex.push(books[7].bookId);
-                
+
                 books[8].bookId = 8;
                 books[8].owner = msg.sender;
                 books[8].temporalOwner = msg.sender;
-                books[8].purchasePrice = 10000000000000;
-                books[8].lendingPrice = 5000000000000;
+                books[8].purchasePrice = 100000000000000;
+                books[8].lendingPrice = 500000000000000;
                 bookIndex.push(books[8].bookId);
-                
+
                 books[9].bookId = 9;
                 books[9].owner = msg.sender;
                 books[9].temporalOwner = msg.sender;
-                books[9].purchasePrice = 10000000000000;
-                books[9].lendingPrice = 5000000000000;
+                books[9].purchasePrice = 100000000000000;
+                books[9].lendingPrice = 500000000000000;
                 bookIndex.push(books[9].bookId);
-                
+
                 books[10].bookId = 10;
                 books[10].owner = msg.sender;
                 books[10].temporalOwner = msg.sender;
-                books[10].purchasePrice = 10000000000000;
-                books[10].lendingPrice = 5000000000000;
+                books[10].purchasePrice = 100000000000000;
+                books[10].lendingPrice = 500000000000000;
                 bookIndex.push(books[10].bookId);
-                
+
                 books[11].bookId = 11;
                 books[11].owner = msg.sender;
                 books[11].temporalOwner = msg.sender;
-                books[11].purchasePrice = 10000000000;
-                books[11].lendingPrice = 5000000000;
+                books[11].purchasePrice = 100000000000000;
+                books[11].lendingPrice = 500000000000000;
                 bookIndex.push(books[11].bookId);
-                
+
                 books[12].bookId = 12;
                 books[12].owner = msg.sender;
                 books[12].temporalOwner = msg.sender;
-                books[12].purchasePrice = 10000000000;
-                books[12].lendingPrice = 5000000000;
+                books[12].purchasePrice = 100000000000000;
+                books[12].lendingPrice = 500000000000000;
                 bookIndex.push(books[12].bookId);
-                
+
                 books[13].bookId = 13;
                 books[13].owner = msg.sender;
                 books[13].temporalOwner = msg.sender;
-                books[13].purchasePrice = 10000000000;
-                books[13].lendingPrice = 5000000000;
+                books[13].purchasePrice = 100000000000000;
+                books[13].lendingPrice = 500000000000000;
                 bookIndex.push(books[13].bookId);
-                
+
                 books[14].bookId = 14;
                 books[14].owner = msg.sender;
                 books[14].temporalOwner = msg.sender;
-                books[14].purchasePrice = 10000000000;
-                books[14].lendingPrice = 5000000000;
+                books[14].purchasePrice = 100000000000000;
+                books[14].lendingPrice = 500000000000000;
                 bookIndex.push(books[14].bookId);
-                
+
                 books[15].bookId = 15;
                 books[15].owner = msg.sender;
                 books[15].temporalOwner = msg.sender;
-                books[15].purchasePrice = 10000000000;
-                books[15].lendingPrice = 5000000000;
+                books[15].purchasePrice = 100000000000000;
+                books[15].lendingPrice = 500000000000000;
                 bookIndex.push(books[15].bookId);
-                
+
                 books[16].bookId = 16;
                 books[16].owner = msg.sender;
                 books[16].temporalOwner = msg.sender;
-                books[16].purchasePrice = 10000000000;
-                books[16].lendingPrice = 5000000000;
+                books[16].purchasePrice = 100000000000000;
+                books[16].lendingPrice = 500000000000000;
                 bookIndex.push(books[16].bookId);
-                
+
                 books[17].bookId = 17;
                 books[17].owner = msg.sender;
                 books[17].temporalOwner = msg.sender;
-                books[17].purchasePrice = 10000000000;
-                books[17].lendingPrice = 5000000000;
+                books[17].purchasePrice = 100000000000000;
+                books[17].lendingPrice = 500000000000000;
                 bookIndex.push(books[17].bookId);
-                
+
                 books[18].bookId = 18;
                 books[18].owner = msg.sender;
                 books[18].temporalOwner = msg.sender;
-                books[18].purchasePrice = 10000000000;
-                books[18].lendingPrice = 5000000000;
+                books[18].purchasePrice = 100000000000000;
+                books[18].lendingPrice = 500000000000000;
                 bookIndex.push(books[18].bookId);
-                
+
                 books[19].bookId = 19;
                 books[19].owner = msg.sender;
                 books[19].temporalOwner = msg.sender;
-                books[19].purchasePrice = 10000000000;
-                books[19].lendingPrice = 5000000000;
+                books[19].purchasePrice = 100000000000000;
+                books[19].lendingPrice = 500000000000000;
                 bookIndex.push(books[19].bookId);
 
-
         // fin inicialización datos.
-
-        
     }
 
 }
